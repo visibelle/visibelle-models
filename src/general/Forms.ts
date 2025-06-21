10b5f233385030ec92df062e0ecfd3cf93187185
iliasvisibelle <ilias@visibelle.be>
2025-06-21 14:56:02 +0200

    'auto'

---
+++ src/general/Forms.ts
@@ -0,0 +1,76 @@
+import BaseModel from "../BaseModel";
+import {
+  Entity,
+  Column,
+  OneToMany,
+  OneToOne,
+  JoinColumn,
+  ManyToOne,
+} from "typeorm";
+import Customer from "./Customer";
+
+export enum FormTypes {
+  CONTACTFORM = "CONTACTFORM",
+}
+
+@Entity({
+  name: "forms",
+})
+export default class Form extends BaseModel {
+  // Relationships
+  @ManyToOne(() => Customer, (customer) => customer.documents)
+  public customer: Customer;
+
+  @Column({
+    nullable: false,
+  })
+  public customerId: number;
+
+  @Column({
+    type: "enum",
+    enum: FormTypes,
+    default: FormTypes.CONTACTFORM,
+    nullable: false,
+  })
+  public type: FormTypes;
+
+  @Column({
+    type: "json", // or "jsonb" for PostgreSQL
+    nullable: true,
+    default: {},
+  })
+  public data: Record<string, any>;
+
+  @Column({
+    type: "json", // or "jsonb" for PostgreSQL
+    nullable: true,
+    default: [],
+  })
+  public attachments: string[]; // or use a more specific type
+
+  constructor(
+    customerId: number,
+    type: FormTypes = FormTypes.CONTACTFORM,
+    data: Record<string, any> = {},
+    attachments: string[] = []
+  ) {
+    super("cust");
+    this.customerId = customerId;
+    this.type = type;
+    this.data = data;
+    this.attachments = attachments;
+  }
+
+  public toJSON() {
+    return {
+      id: this.id,
+      key: this.key,
+      createdOn: this.createdOn,
+      active: this.active,
+      customerId: this.customerId,
+      type: this.type,
+      data: this.data ?? {},
+      attachments: this.attachments ?? [],
+    };
+  }
+}
