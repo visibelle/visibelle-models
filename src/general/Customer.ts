5f06ba31e958536db7b2eecc97224465d4685b3f
iliasvisibelle <ilias@visibelle.be>
2025-06-21 15:48:42 +0200

    'auto'

--- src/general/Customer.ts
+++ src/general/Customer.ts
@@ -5,93 +5,102 @@ import Address from "./Address";
 import Document from "./Document";
 
 @Entity({
-    name: "customers"
+  name: "customers",
 })
 export default class Customer extends BaseModel {
+  @Column()
+  public activity: string;
 
-    @Column()
-    public activity: string;
+  @Column({ nullable: true })
+  public addressId: number;
 
-    @Column({ nullable: true })
-    public addressId: number;
+  @Column({ nullable: true })
+  public phoneFix: string;
 
-    @Column({ nullable: true })
-    public phoneFix: string;
+  @Column({ nullable: true })
+  public phoneMobile: string;
 
-    @Column({ nullable: true })
-    public phoneMobile: string;
+  @Column({ nullable: true })
+  public vatId: string;
 
-    @Column({ nullable: true })
-    public vatId: string;
+  @Column({ nullable: true })
+  public IBAN: string;
 
-    @Column({ nullable: true })
-    public IBAN: string;
+  @Column()
+  public ownerEmail: string;
 
-    @Column()
-    public ownerEmail: string;
+  @Column({
+    nullable: false,
+  })
+  public companyName: string;
 
-    @Column()
-    public ownerFirstName: string;
+  @Column()
+  public ownerFirstName: string;
 
-    @Column()
-    public ownerLastName: string;
+  @Column()
+  public ownerLastName: string;
 
-    @Column()
-    public companyForm: string;
+  @Column()
+  public companyForm: string;
 
-    // Relationships
-    @OneToMany(() => User, user => user.customer)
-    public users: User[];
+  // Relationships
+  @OneToMany(() => User, (user) => user.customer)
+  public users: User[];
 
-    @OneToOne(() => Address, address => address.customer)
-    @JoinColumn({ name: "addressId" })
-    public address: Address;
+  @OneToOne(() => Address, (address) => address.customer)
+  @JoinColumn({ name: "addressId" })
+  public address: Address;
 
-    @OneToMany(() => Document, document => document.customer)
-    public documents: Document[];
+  @OneToMany(() => Document, (document) => document.customer)
+  public documents: Document[];
 
-    constructor(
-        activity: string,
-        ownerEmail: string,
-        ownerFirstName: string,
-        ownerLastName: string,
-        companyForm: string,
-        phoneFix?: string,
-        phoneMobile?: string,
-        vatId?: string,
-        IBAN?: string
-    ) {
-        super("cust");
-        this.activity = activity;
-        this.ownerEmail = ownerEmail;
-        this.ownerFirstName = ownerFirstName;
-        this.ownerLastName = ownerLastName;
-        this.companyForm = companyForm;
-        this.phoneFix = phoneFix;
-        this.phoneMobile = phoneMobile;
-        this.vatId = vatId;
-        this.IBAN = IBAN;
-    }
+  constructor(
+    companyName: string,
+    activity: string,
+    ownerEmail: string,
+    ownerFirstName: string,
+    ownerLastName: string,
+    companyForm: string,
+    phoneFix?: string,
+    phoneMobile?: string,
+    vatId?: string,
+    IBAN?: string
+  ) {
+    super("cust");
+    this.companyName = companyName;
+    this.activity = activity;
+    this.ownerEmail = ownerEmail;
+    this.ownerFirstName = ownerFirstName;
+    this.ownerLastName = ownerLastName;
+    this.companyForm = companyForm;
+    this.phoneFix = phoneFix;
+    this.phoneMobile = phoneMobile;
+    this.vatId = vatId;
+    this.IBAN = IBAN;
+  }
 
-    public toJSON() {
-        return {
-            id: this.id,
-            key: this.key,
-            createdOn: this.createdOn,
-            active: this.active,
-            activity: this.activity,
-            addressId: this.addressId,
-            phoneFix: this.phoneFix,
-            phoneMobile: this.phoneMobile,
-            vatId: this.vatId,
-            IBAN: this.IBAN,
-            ownerEmail: this.ownerEmail,
-            ownerFirstName: this.ownerFirstName,
-            ownerLastName: this.ownerLastName,
-            companyForm: this.companyForm,
-            address: this.address ? this.address.toJSON() : null,
-            documents: this.documents ? this.documents.map(doc => doc.toJSON()) : [],
-            users: this.users ? this.users.map(user => user.toJSON()) : []
-        };
-    }
+  public toJSON() {
+    return {
+      id: this.id,
+      key: this.key,
+      createdOn: this.createdOn,
+      companyName: this.companyName,
+      active: this.active,
+      activity: this.activity,
+      addressId: this.addressId,
+      phoneFix: this.phoneFix,
+      phoneMobile: this.phoneMobile,
+      vatId: this.vatId,
+      IBAN: this.IBAN,
+      ownerEmail: this.ownerEmail,
+      ownerFirstName: this.ownerFirstName,
+      ownerLastName: this.ownerLastName,
+      companyForm: this.companyForm,
+      address: this.address ? this.address.toJSON() : null,
+      documents: this.documents
+        ? this.documents.map((doc) => doc.toJSON())
+        : [],
+      users: this.users ? this.users.map((user) => user.toJSON()) : [],
+    };
+  }
 }
