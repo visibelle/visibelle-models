2bcfb18138d962507581c00709a811d1d9e9522a
iliasvisibelle <ilias@visibelle.be>
2025-11-07 14:03:43 +0100

    'auto'

--- src/general/Document.ts
+++ src/general/Document.ts
@@ -3,49 +3,49 @@ import { Entity, Column, ManyToOne } from "typeorm";
 import Customer from "./Customer";
 
 @Entity({
-    name: "documents"
+  name: "documents",
 })
 export default class Document extends BaseModel {
+  @Column()
+  public type: string;
 
-    @Column()
-    public type: string;
+  @Column({ nullable: true })
+  public extraInfo: string;
 
-    @Column({ nullable: true })
-    public extraInfo: string;
+  @Column()
+  public url: string;
 
-    @Column()
-    public url: string;
+  @Column()
+  public fileSize: number;
 
-    @Column()
-    public fileSize: number;
+  // Relationships
+  @ManyToOne(() => Customer, (customer) => customer.documents)
+  public customer: Customer;
 
-    // Relationships
-    @ManyToOne(() => Customer, customer => customer.documents)
-    public customer: Customer;
+  @Column()
+  public customerId: number;
 
-    constructor(
-        type: string,
-        url: string,
-        fileSize: number,
-        extraInfo?: string
-    ) {
-        super("doc");
-        this.type = type;
-        this.url = url;
-        this.fileSize = fileSize;
-        this.extraInfo = extraInfo;
-    }
+  constructor(customerId: number, type: string, url: string, fileSize: number, extraInfo?: string) {
+    super("doc");
+    this.customerId = customerId;
+    this.type = type;
+    this.url = url;
+    this.fileSize = fileSize;
+    this.extraInfo = extraInfo;
 
-    public toJSON() {
-        return {
-            id: this.id,
-            key: this.key,
-            createdOn: this.createdOn,
-            active: this.active,
-            type: this.type,
-            extraInfo: this.extraInfo,
-            url: this.url,
-            fileSize: this.fileSize
-        };
-    }
+  }
+
+  public toJSON() {
+    return {
+      id: this.id,
+      key: this.key,
+      createdOn: this.createdOn,
+      active: this.active,
+      customerId: this.customerId,
+      type: this.type,
+      extraInfo: this.extraInfo,
+      url: this.url,
+      fileSize: this.fileSize,
+    };
+  }
 }
