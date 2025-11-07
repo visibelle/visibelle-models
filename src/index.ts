360b361cb75ddb1f1753fd440f17e337936cb227
iliasvisibelle <ilias@visibelle.be>
2025-11-07 13:22:56 +0100

    'auto'

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,30 @@ import Document from "./general/Document";
 import User from "./general/User";
 import { UserCoreRole, DefaultLocales } from "./general/User";
 import Token, { TokenType } from "./general/Token";
+import Form, { FormTypes } from "./general/Forms";
+import Config, {
+  ConfigDataMap,
+  ConfigType,
+  CustomerConfigData,
+  ContactFormConfigData,
+  BookingConfirmConfigData,
+} from "./general/Config";
 
 export {
-    Address,
-    Customer,
-    Document,
-    User,
-    UserCoreRole,
-    DefaultLocales,
-    Token,
-    TokenType
-}
+  Address,
+  Customer,
+  Document,
+  User,
+  UserCoreRole,
+  DefaultLocales,
+  Token,
+  TokenType,
+  FormTypes,
+  Form,
+  Config,
+  ConfigDataMap,
+  ConfigType,
+  CustomerConfigData,
+  ContactFormConfigData,
+  BookingConfirmConfigData,
+};
